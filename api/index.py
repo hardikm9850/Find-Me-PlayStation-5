130b1c89a540209010207ef302d7456d30aa5017
Hardik Mehta <hardik@teachmint.com>
2022-05-09 10:07:25 +0530

    index file refactored

--- api/index.py
+++ api/index.py
@@ -2,22 +2,37 @@ from http.server import BaseHTTPRequestHandler
 from urllib import parse
 from datetime import datetime
 import threading
-import FindTweets
 
 class handler(BaseHTTPRequestHandler):
 
-	def do_GET(self):
-		s = self.path
-		dic = dict(parse.parse_qsl(parse.urlsplit(s).query))
-		self.send_response(200)
-		self.send_header('Content-type','text/plain')
-		self.end_headers()
 
-		if "name" in dic:
-			message = "Hello, " + dic["name"] + "!"
-		else:
-			message = "Hello, stranger!"
+  def checkTime():
+    # This function runs periodically every 1 second
+    threading.Timer(1, checkTime).start()
 
-		self.wfile.write(message.encode())
-		#checkTime()
-		return
+    now = datetime.now()
+
+    current_time = now.strftime("%H:%M:%S")
+    print("Current Time =", current_time)
+
+    if(current_time == '07:00:00'):  # check if matches with the desired time
+        print('checking tweets')
+        FindTweets.get_tweets()
+
+    return
+
+  def do_GET(self):
+    s = self.path
+    dic = dict(parse.parse_qsl(parse.urlsplit(s).query))
+    self.send_response(200)
+    self.send_header('Content-type','text/plain')
+    self.end_headers()
+
+    if "name" in dic:
+      message = "Hello, " + dic["name"] + "!"
+    else:
+      message = "Hello, stranger!"
+
+    self.wfile.write(message.encode())
+    checkTime()
+    return
